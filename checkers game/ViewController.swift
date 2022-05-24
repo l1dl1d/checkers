2e8a719af410c0b5045793c1e13b8fa82075b297
kjaveed <javeekh@ssfs.org>
2022-05-24 09:54:33 -0400

    started cresting outlets and ran into a problem

--- checkers game/ViewController.swift
+++ checkers game/ViewController.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet func square0(_ sender: UIButton) {
+    }
+    @IBOutlet func square1(_ sender: UIButton) {
+    }
+    @IBOutlet func square2(_ sender: UIButton) {
+    }
+    @IBOutlet func square3(_ sender: UIButton) {
+    }
+    @IBOutlet func square4(_ sender: UIButton) {
+    }
+    @IBOutlet func square5(_ sender: UIButton) {
+    }
+    @IBOutlet func square6(_ sender: UIButton) {
+    }
+    @IBOutlet func square7(_ sender: UIButton) {
+    }
+    @IBOutlet func square8(_ sender: UIButton) {
+    }
+    @IBOutlet func square9(_ sender: UIButton) {
+    }
+    @IBOutlet func square10(_ sender: UIButton) {
+    }
+    @IBOutlet func square11(_ sender: UIButton) {
+    }
+    @IBOutlet func square12(_ sender: UIButton) {
+    }
+    @IBOutlet func square13(_ sender: UIButton) {
+    }
+    @IBOutlet func square14(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
